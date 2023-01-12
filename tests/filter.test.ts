90f46f3027bdf3f25dd677d17dc2b9bd738db0ed
MaryAnzh <maryang@mail.ru>
2023-01-12 21:43:14 +0300

    feat: add filter test-2

--- tests/filter.test.ts
+++ tests/filter.test.ts
@@ -2,15 +2,16 @@ import { toolsModel } from '../src/core/model/toolsModel';
 import { catalog } from '../src/core/data/catalog.data';
 import { IItem } from '../src/core/interfaces/catalog.interfaces';
 
+const items = catalog.products.splice(0, 4);
+
 describe('Filter items by key word', () => {
-    const items = catalog.products.splice(0, 4);
 
     test('Filter by Brand', () => {
         const key = 'brand' as keyof IItem;
         const namesForTest1 = 'Apple';
         const namesForTest2 = 'Samsung';
         const namesForTest3 = 'OPPO';
-        
+
         expect(toolsModel.filterItems(namesForTest1, key, items).length).toBe(2);
         expect(toolsModel.filterItems(namesForTest2, key, items).length).toBe(1);
         expect(toolsModel.filterItems(namesForTest3, key, items).length).toBe(1);
@@ -24,5 +25,15 @@ describe('Filter items by key word', () => {
         expect(toolsModel.filterItems(namesForTest1, key, items).length).toBe(4);
         expect(toolsModel.filterItems(namesForTest2, key, items).length).toBe(0);
         expect(toolsModel.filterItems(namesForTest3, key, items).length).toBe(0);
+    });
+});
+
+describe('Filter items by filtering param', () => {
+    test('Create category by Brand', () => {
+        const namesForTest1 = 'category';
+        const namesForTest2 = 'brand';
+
+        expect(toolsModel.createSelectViewByToolsTitle(namesForTest1, items).length).toBe(1);
+        expect(toolsModel.createSelectViewByToolsTitle(namesForTest2, items).length).toBe(3);
     });
 });
